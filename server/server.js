115e017060cf0a501ca5dbf78485de31e406f8f5
unknown <gokul5a738@gmail.com>
2024-07-09 21:43:47 +0530

    gg

--- server/server.js
+++ server/server.js
@@ -27,9 +27,6 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
 });
 const PORT = process.env.PORT || 3001;
-
-// server.listen(PORT, () => {
-//   console.log("Server is running on port 3001");
-// });
-
-module.exports = app;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
